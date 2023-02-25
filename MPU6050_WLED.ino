25914d727c678decde9154840c64927f5a05826d
William Lucid <11734452+Tech500@users.noreply.github.com>
2023-02-25 07:06:30 -0500

    Update MPU6050_WLED.ino

--- MPU6050_WLED.ino
+++ MPU6050_WLED.ino
@@ -2,7 +2,7 @@
 /*
     Developed by William Lucid with an assist from OpenAI's ChatGPT 02/24/2023  Only partially finished; has not been added directly to running WLED, project 
     is a work-in-progress.  Will need a usermod to be added and compiled for WLED.  Sketch was developed to generate varialbles for effects, Intensity, and color 
-    palette variables of WLED project.  WLED Project:  https://kno.wled.ge/  
+    palette variables of WLED project.  WLED Project:  https://kno.wled.ge/  WLED runs on ESP8266 or ESP32.
     
     WLED "List of Effects and Palettes":  https://github.com/Aircoookie/WLED/wiki/List-of-effects-and-palettes 
 
