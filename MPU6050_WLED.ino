5a643b3bffe9eeb3b4d82bcc5565d5ebf82024d1
William Lucid <11734452+Tech500@users.noreply.github.com>
2023-03-22 03:14:48 -0400

    Updated 03/22/2023

--- MPU6050_WLED.ino
+++ MPU6050_WLED.ino
@@ -1,6 +1,6 @@
 /*
 
-    "MPU6050_WLED_with_Deep_Sleep.ino"
+    "MPU6050_WLED.ino"
 
     Developed by William Lucid with an assist from OpenAI's ChatGPT 03/10/2023 @ 01:00 EST.  Only partially finished; has not been added directly to running WLED, project 
     is a work-in-progress.  Will need a usermod to be added and compiled for WLED.  Sketch was developed to generate varialbles for effects, Intensity, and color 
