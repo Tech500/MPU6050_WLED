fea4945fcae10a2e2174906501a7612e739737f0
William Lucid <11734452+Tech500@users.noreply.github.com>
2023-04-15 09:18:05 -0400

    Add files via upload

---
+++ MPU6050_WLED_withFive_Switches/network.h
@@ -0,0 +1,18 @@
+const char* ssid = "R2D2";
+const char* password = "sissy4357";
+
+// Change the IP address and port number to match WLED Controller
+String WLED_IP_ADDRESS = "http://10.0.0.9";
+const int port = 80;
+
+WiFiServer server1(port);
+
+/*
+  HttpClient server1 settings
+  default custom static IP
+  WiFi Settings for WiFi Manager
+*/
+char static_ip[16] = "10.0.0.23";
+char static_gw[16] = "10.0.0.1";
+char static_sn[16] = "255.255.255.0";
+char static_dns[16] = "10.0.0.1";
